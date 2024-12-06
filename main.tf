ddd08e439153c119581021df29a496692130b06e
jasiu777 <jacek.siutkowski@gmail.com>
2024-12-06 14:01:02 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,8 +18,8 @@ resource "aws_instance" "app" {
 #  }
 #}
 
-resource "aws_key_pair" "kurs" {
-  key_name   = "kurs-key"
+resource "aws_key_pair" "terraform" {
+  key_name   = "terraform-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwOnTVCci4sIM4Gt52UuqwapklWlMy4eogKOsdlmRc0b9KyM4QrqqWrDNMt1MvII1yIfM0GvvyAS0UOGR5QUfhvNmaHZY1pKH0s0DooFEkYSrUQpeznxozcpcWQ3ko1HG2O66T+VVRrhVgSrgpfGPacnJqjc7B1lLu/YaAPR0TGIPV0nYWr+XSAuFbpiNl124MAfR2oHOdOY5UO0aGFIuzyyHnxN1m9TekRvOhOnYx/gu+3WRvf2AQkSdZMCdEsXgkqcYxhaf3rfl5aELsuEze+sEEyJqADP/jtrfcezsVGNYO/LH0bqqx4LtxPDzvKJcObetjZK4ayFF5e/Acpk/F9HIccVcB96yKKg3PVVr8cCv/mAQ5uzbTgbxcY8AxOt9H3GVcHu5xB7uPtSgOLOn0cYGI3COClwYHrpKc+zN2zNaTXLvpYBu0sLuhFNmdx+nfaxMMysJb/M5n3kqyI6UsHmyAHpz0X6hpKrSiWAtYvXSDQcwitRjKixR3Ei9NQRk= kurs@01UVMDev"
 }
 
