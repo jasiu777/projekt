d0c0ba45989cacb453990ee3711f1f3e6231f883
jasiu777 <jacek.siutkowski@gmail.com>
2024-12-06 14:09:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,19 @@ resource "aws_instance" "app" {
 #  }
 #}
 
+  provisioner "remote-exec" {
+    inline = [
+      "sudo chown ec2-user:ec2-user /home/ec2-user/.ssh/authorized_keys",
+      "sudo chmod 0600 /home/ec2-user/.ssh/authorized_keys"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      host        = self.public_ip
+    }
+  }
+
 resource "aws_key_pair" "ec2-user" {
   key_name   = "ec2-user"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCwOnTVCci4sIM4Gt52UuqwapklWlMy4eogKOsdlmRc0b9KyM4QrqqWrDNMt1MvII1yIfM0GvvyAS0UOGR5QUfhvNmaHZY1pKH0s0DooFEkYSrUQpeznxozcpcWQ3ko1HG2O66T+VVRrhVgSrgpfGPacnJqjc7B1lLu/YaAPR0TGIPV0nYWr+XSAuFbpiNl124MAfR2oHOdOY5UO0aGFIuzyyHnxN1m9TekRvOhOnYx/gu+3WRvf2AQkSdZMCdEsXgkqcYxhaf3rfl5aELsuEze+sEEyJqADP/jtrfcezsVGNYO/LH0bqqx4LtxPDzvKJcObetjZK4ayFF5e/Acpk/F9HIccVcB96yKKg3PVVr8cCv/mAQ5uzbTgbxcY8AxOt9H3GVcHu5xB7uPtSgOLOn0cYGI3COClwYHrpKc+zN2zNaTXLvpYBu0sLuhFNmdx+nfaxMMysJb/M5n3kqyI6UsHmyAHpz0X6hpKrSiWAtYvXSDQcwitRjKixR3Ei9NQRk= ec2-user@01UVMDev"
