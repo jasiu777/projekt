1e0b190ade9ec36433780607cf9ab21f1eaaa02d
jasiu777 <jacek.siutkowski@gmail.com>
2024-12-06 13:51:43 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,26 +9,7 @@ resource "aws_instance" "app" {
   tags = {
     Name = "PythonAppInstance"
   }
-
-  provisioner "remote-exec" {
-    inline = [
-        "sudo adduser --disabled-password --gecos '' myuser",
-        "sudo mkdir -p /home/myuser/.ssh",
-        "sudo touch /home/myuser/.ssh/authorized_keys",
-        "sudo echo '${var.MY_USER_PUBLIC_KEY}' > authorized_keys",
-        "sudo mv authorized_keys /home/myuser/.ssh",
-        "sudo chown -R myuser:myuser /home/myuser/.ssh",
-        "sudo chmod 700 /home/myuser/.ssh",
-        "sudo chmod 600 /home/myuser/.ssh/authorized_keys",
-        "sudo usermod -aG sudo myuser"
-   ]
-
-    connection {
-     user     = "ubuntu"
-    }
-
-  }
-  }
+}
 
 #  provisioner "remote-exec" {
 #    inline = [
