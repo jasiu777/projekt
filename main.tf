1838d13ad3b1a3f82b146f7fbc2cf79706b98726
jasiu777 <jacek.siutkowski@gmail.com>
2024-12-06 11:15:21 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_instance" "app" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "PythonAppInstance"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker run -d -p 80:80 python-app"
+    ]
+  }
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "ami_id" {
+  default = "ami-0c02fb55956c7d316" # AMI z Linux
+}
